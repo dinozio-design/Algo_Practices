718db7665954eacc46e1372bccd548ab2dcefd29
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-04 15:39:47 -0500

    cleaned up the code for 01

--- 01_CodelandUsernameValidation/script.js
+++ 01_CodelandUsernameValidation/script.js
@@ -7,8 +7,11 @@ function CodelandUsernameValidation(str) {
         -   [a-zA-Z0-9_]{2,23}: Matches between 2 and 23 characters consisting of letters, numbers, and underscores.
         -   [a-zA-Z0-9]$: Ensures that the username does not end with an underscore.
     */
+    
+    // this arrow function test if the input `str` is between 4 and 25 characters
+    const validateLength = (str) => str.length >= 4 && str.length <= 25;
 
-    return validateRegEx.test(str) && str.length >= 4 && str.length <= 25;
+    return validateRegEx.test(str) && validateLength(str);
     // return str; 
 
 };
