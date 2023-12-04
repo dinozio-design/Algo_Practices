6ebcd96478607a2507a8c8111d74abf1467f781a
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-04 13:22:27 -0500

    second challnege BigO notes

--- 02_FindIntersection/script.js
+++ 02_FindIntersection/script.js
@@ -23,3 +23,14 @@ function FindIntersection(strArr) {
     // return outPut;
   
   }
+
+  /*
+  Calculatingruntime...
+Above will be the running time of your algorithm expressed in Big-O notation. Big-O notation is used to classify algorithms according to how their run time grows as the input size grows. To learn more here is a video we published on this topic, and here is a guide on how it works.
+
+`https://www.youtube.com/watch?v=HfIH3czXc-8`
+`https://coderbyte.com/big-o-runtime-guide`
+`https://en.wikipedia.org/wiki/Big_O_notation#Orders_of_common_functions`
+
+
+*/
