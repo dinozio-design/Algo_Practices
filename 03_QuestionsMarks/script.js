df6a4d128fa5f552fc04ebca50840e21856142c0
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-05 17:02:23 -0500

    i fixed it to count EXACTLY 3 '?'

--- 03_QuestionsMarks/script.js
+++ 03_QuestionsMarks/script.js
@@ -1,18 +1,33 @@
 function QuestionsMarks(str) {
-    var res = false;
+    // var res = false;
 
+    function countQuestionMarks(arr) {
+        let qMarkCount = 0;
+        for (const element of arr) {
+            if (element === "?") {
+                qMarkCount++;
+            };
+        };
+        // console.log(qMarkCount);
+        return qMarkCount;
+    }
     // Iterate through the string
-    for (i=0; i<str.length; i++){
-        for (j=i+1; j<str.length; j++){
+    for (i = 0; i < str.length; i++) {
+        for (j = i + 1; j < str.length; j++) {
             // Check if the sum of the two digits is 10
-            if (Number(str[i])+Number(str[j]) === 10){
-                res = true;
-                // cound the Q Marks between the two indexes
-                if(str.slice(i,j).split("?").length - 1 <3){
-                    return false;
-                };
+            if (Number(str[i]) + Number(str[j]) === 10) {
+                let newArr = str.slice(i, j).split("");
+                // console.log(newArr);
+                if (countQuestionMarks(newArr) === 3) {
+                    return true;
+                }
             };
         };
     };
-    return res;
+    return false;
 }
+
+console.log(QuestionsMarks("arrb6????4xxbl5??d?eee5"));
+
+// this code is incomplete and makes mistake. It does not return EXACTLY 3 qs, but AT LEAST
+// I fixed it
