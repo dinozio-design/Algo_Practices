0955b7d3f506122ee07c82de671fa380c73b4e03
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-05 17:52:34 -0500

    fixed it to perfection

--- 03_QuestionsMarks/script.js
+++ 03_QuestionsMarks/script.js
@@ -1,6 +1,5 @@
 function QuestionsMarks(str) {
-    // var res = false;
-
+    const results = [];
     function countQuestionMarks(arr) {
         let qMarkCount = 0;
         for (const element of arr) {
@@ -19,15 +18,29 @@ function QuestionsMarks(str) {
                 let newArr = str.slice(i, j).split("");
                 // console.log(newArr);
                 if (countQuestionMarks(newArr) === 3) {
-                    return true;
-                }
+                    results.push(true);
+                } else {
+                    results.push(false);
+                };
+                i = j;
             };
         };
     };
-    return false;
+    // console.log(results);
+    if (results.length === 0) {
+        return false;
+    };
+    // console.log(results);
+    return !results.includes(false);
+
 }
 
-console.log(QuestionsMarks("arrb6????4xxbl5??d?eee5"));
+// console.log(QuestionsMarks("aa6?9"));
+// console.log(QuestionsMarks("arrb6???4xxbl5??d?eee5"));
+// console.log(QuestionsMarks("9???1???9??1???9"));
+// console.log(QuestionsMarks("9???1???9???1???9"));
+// console.log(QuestionsMarks("5??aaaaaaaaaaaaaaaaaaa?5?5"));
+// console.log(QuestionsMarks("5??aaaaaaaaaaaaaaaaaaa?5?a??5"));
 
 // this code is incomplete and makes mistake. It does not return EXACTLY 3 qs, but AT LEAST
 // I fixed it
